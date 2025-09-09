98d66c674b6b7f022bd9df062d651f27111678e2
BerserkWInter <dharneshh.bala@gmail.com>
2025-09-09 21:52:13 +0530

    init

--- srcs/Arithmetic.v
+++ srcs/Arithmetic.v
@@ -1,9 +1,9 @@
 module Arithmetic (
     input [31:0] x,
-    input [31:0] y,
-    output [31:0] A_out
+    input [31:0] y_input,
+    input add_sub,
+    output [31:0] A_out,
+    output cout
 );
-    
-    f
-
+    fulladderN(.a(x),.b(y_input),.cin(add_sub),.cout(cout),.sum(A_out));
 endmodule
