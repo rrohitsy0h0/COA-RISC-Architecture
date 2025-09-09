c5da442de88644352b424ed0da7f05cea44caf9c
rrohitsy0h0 <rrohitsatpute@gmail.com>
2025-09-09 20:56:47 +0530

    cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define mod 1000000007
+#define vi vector<long long>
+#define pb push_back
+#define pp pop_back
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define endl "\n"
+
+using state=pair<int,int>;
+const int init=1e9;
+
+void check(int);
+void solve(int);
+
+void pre(){
+    return;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    pre();
+    int t=1;
+    cin>>t;while(t--)
+    solve(t),cout<<endl;
+    return 0;
+}
+
+void check(int n){
+    cout<<"Check - "<<n<<endl;
+    return;
+}
+
+void solve(int testcase){
+    //cout<<"Test -  "<<testcase<<endl;  
+    
+    return;
+}
