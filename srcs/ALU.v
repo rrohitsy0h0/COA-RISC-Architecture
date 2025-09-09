b99478ac80262373e3bfaa7f4c7fe8385e9df4fc
BerserkWInter <dharneshh.bala@gmail.com>
2025-09-09 22:26:28 +0530

    Update ALU.v

--- srcs/ALU.v
+++ srcs/ALU.v
@@ -4,14 +4,20 @@ module ALU (
     input [31:0] x,
     input [31:0] y,
     input add_sub,
-    input [4:0] ALU_op,
+    input [1:0] LogicFn,
+    input [1:0] fn,
     output [31:0] ALU_result,
     output Overflow,
     output Zero,
     output Sign
 );
 
-    Arithmetic A();
+    Arithmetic A(
+        .x(x),
+        .y_input(y_input),
+        .add_sub(add_sub),
+        .A_out($$),
+        .cout());
 
     Logical L();
 
