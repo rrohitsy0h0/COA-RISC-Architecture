d047b3606027525cfcd529773e97bf6027993b8e
BerserkWInter <dharneshh.bala@gmail.com>
2025-09-09 23:31:04 +0530

    ALU_draft1

--- srcs/ALU.v
+++ srcs/ALU.v
@@ -5,24 +5,49 @@ module ALU (
     input [31:0] y,
     input add_sub,
     input [1:0] LogicFn,
-    input [1:0] fn,
+    input [1:0] FnClass,
     output [31:0] ALU_result,
-    output Overflow,
+    output Overflow
 );
 
     wire C_Flag,Z_Flag,S_Flag,V_Flag;
     wire [31:0] F;
-    Arithmetic A(
+    wire [31:0] L;
+    wire [31:0] LUI16;
+    wire [31:0] sl;
+
+    assign y_input = (add_sub==1'b1) ? ~y : y; //XOR of y and add_sum
+
+    Arithmetic A( // Arithmetic Unit
         .x(x),
         .y_input(y_input),
         .add_sub(add_sub),
-        .A_out($$),
-        .cout());
-
-    logic L(
-        .x(x),.y(y),.logicfn(LogicFn),.l_output(F)
+        .A_out(F),
+        .cout(C_Flag)
     );
 
-    Mux M();
+    assign Z_Flag = ~|F;
+    assign S_Flag = F[31];
+    assign V_Flag = C_Flag ^ S_Flag;
+    assign Overflow = V_Flag;
+
+    logic L( // Logical Unit
+        .x(x),
+        .y(y),
+        .logicfn(LogicFn),
+        .l_output(L)
+    );
+
+    assign LUI16 = {y[15:0],16'b0};
+    assign sl = (S_Flag==1'b0) ? 32'b0 : 32'b1;
+
+    Fn_mux M(
+        .LUI16(LUI16),
+        .sl(sl),
+        .A_out(F),
+        .L_out(L),
+        .FnClass(FnClass),
+        .Result(ALU_result)
+    );
     
 endmodule
---
+++ srcs/Fn_mux.v
@@ -0,0 +1,15 @@
+module Fn_mux(
+    input [31:0] LUI16,
+    input [31:0] sl,
+    input [31:0] A_out,
+    input [31:0] L_out,
+    input [1:0] FnClass,
+    output Result
+);
+
+assign Result=(FnClass==2'b00) ? LUI16 : 32'b0;
+assign Result=(FnClass==2'b01) ? sl : 32'b0;
+assign Result=(FnClass==2'b10) ? A_out : 32'b0;
+assign Result=(FnClass==2'b11) ? L_out : 32'b0;
+
+endmodule
