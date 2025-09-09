2607c1fb007ed78bdda0c40e48852918880fdb89
rrohitsy0h0 <rrohitsatpute@gmail.com>
2025-09-09 21:20:23 +0530

    1 bit

---
+++ srcs/fulladder1.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module fulladder1(
+    input a,b,cin,
+    output cout,sum
+);
+
+wire w1,w2,w3;
+xor(w1,a,b);
+xor(sum,w1,cin);
+and(w2,a,b);
+and(w3,w1,cin);
+or(cout,w2,w3);
+
+endmodule
